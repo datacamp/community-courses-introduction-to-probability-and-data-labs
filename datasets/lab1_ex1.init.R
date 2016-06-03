c0623a05b9d7d4d338bce777d4b056820e52c504
cam-martin <cameron@datacamp.com>
2016-06-03 13:35:09 -0400

    Update lab1_ex1.init.R

--- datasets/lab1_ex1.init.R
+++ datasets/lab1_ex1.init.R
@@ -1,8 +1,7 @@
 # open file in editor
-file.edit("lab1_ex7.Rmd")
+file.edit("lab1_ex1.Rmd")
 
 # load packages
 library(dplyr)
 library(ggplot2)
 library(statsr)
-load(url("http://s3.amazonaws.com/assets.datacamp.com/production/course_1161/datasets/present_ex6.RData"))
