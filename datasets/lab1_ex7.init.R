fe217ca2af78de0e4110ea4e5e1cd9a6a1fabf69
cam-martin <cameron@datacamp.com>
2016-06-03 10:49:19 -0400

    Update lab1_ex7.init.R

--- datasets/lab1_ex7.init.R
+++ datasets/lab1_ex7.init.R
@@ -8,4 +8,4 @@ library(statsr)
 load(url("http://s3.amazonaws.com/assets.datacamp.com/production/course_1161/datasets/present_ex6.RData"))
 
 # create plot
-ggplot(data = arbuthnot, aes(x = year, y = boys/total)) + geom_line() + geom_point()
+#ggplot(data = arbuthnot, aes(x = year, y = boys/total)) + geom_line() + geom_point()
