b4412cd10654f33f77516e9072932de3d6c53cb7
cam-martin <cameron@datacamp.com>
2016-06-07 13:31:51 -0400

    Update lab1_ex7.init.R

--- datasets/lab1_ex7.init.R
+++ datasets/lab1_ex7.init.R
@@ -6,3 +6,6 @@ library(dplyr)
 library(ggplot2)
 library(statsr)
 load(url("http://s3.amazonaws.com/assets.datacamp.com/production/course_1161/datasets/present_ex6.RData"))
+
+# Knit Rmd file
+rmarkdown::render("lab1_ex7.Rmd")
