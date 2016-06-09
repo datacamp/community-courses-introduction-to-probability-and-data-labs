500774ad48a4b4445baa65bc7355e24122bd729d
cam-martin <cameron@datacamp.com>
2016-06-09 13:15:09 -0400

    Delete lab1_ex6.init.R

--- datasets/lab1_ex6.init.R
+++
@@ -1,8 +0,0 @@
-# open file in editor
-file.edit("lab1_ex6.Rmd")
-
-# load packages
-library(dplyr)
-library(ggplot2)
-library(statsr)
-load(url("http://s3.amazonaws.com/assets.datacamp.com/production/course_1161/datasets/present_ex5.RData"))
