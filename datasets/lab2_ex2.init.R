6bea0817d3a77a05ea0483e9285054de4f8eb85b
cam-martin <cameron@datacamp.com>
2016-06-29 13:05:25 -0400

    Update lab2_ex2.init.R

--- datasets/lab2_ex2.init.R
+++ datasets/lab2_ex2.init.R
@@ -5,3 +5,7 @@ file.edit("lab2_ex2.Rmd")
 library(dplyr)
 library(ggplot2)
 library(statsr)
+
+# creating sfo_feb_flights data frame form previous exercise
+sfo_feb_flights <- nycflights %>%
+  filter(dest == "SFO", month == 2)
