a276173a93ef62b736ae4480caea4b63004b0a62
Nick Carchedi <nick.carchedi@gmail.com>
2019-03-25 15:46:35 -0400

    fix requirements again

--- requirements.r
+++ requirements.r
@@ -1,6 +1,5 @@
 library(remotes)
-# install_version("mvtnorm", "1.0-10")
-install_version("BayesFactor", "0.9.12-4.2", dependencies = TRUE, upgrade = TRUE)
+install_version("mvtnorm", "1.0-10")
 install_version("statsr", "0.1-0", dependencies = TRUE, upgrade = TRUE)
 # install_version("dplyr", "0.8.0.1")
 # install_version("ggplot2", "3.1.0")
--- requirements.sh
+++ requirements.sh
@@ -6,3 +6,6 @@ wget https://assets.datacamp.com/production/course_1156/datasets/all-files.zip
 
 # unzip all files
 unzip all-files.zip -d $DATADIR/
+
+# install some other requirements
+apt-get update && apt-get --yes --force-yes install libxml2-dev libcairo2-dev libgdal-dev
