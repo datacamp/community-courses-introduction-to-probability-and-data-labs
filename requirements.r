ef65f7e0464e0794569e25a04e1e34c1841f4d34
Nick Carchedi <nick.carchedi@gmail.com>
2019-03-25 16:23:52 -0400

    use older mvtnorm

--- requirements.r
+++ requirements.r
@@ -1,5 +1,6 @@
 library(remotes)
-install_version("mvtnorm", "1.0-10")
+# Use last mvtnorm that allowed R 3.4
+install_github("cran/mvtnorm", ref = "39daaf201178042e8c1adde29565d96e5424e21a")
 install_version("statsr", "0.1-0", dependencies = TRUE, upgrade = TRUE)
 # install_version("dplyr", "0.8.0.1")
 # install_version("ggplot2", "3.1.0")
