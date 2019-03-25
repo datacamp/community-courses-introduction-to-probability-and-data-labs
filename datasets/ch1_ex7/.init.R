c66ff303e95326e1c1ff0b4f2b788b48b7fadb19
Nick Carchedi <nick.carchedi@gmail.com>
2019-03-25 14:06:40 -0400

    reorg datasets/

--- datasets/ch1_ex7/.init.R
+++ datasets/ch1_ex7/.init.R
@@ -5,7 +5,7 @@ file.edit("lab1_ex7.2.Rmd")
 library(dplyr)
 library(ggplot2)
 library(statsr)
-load(url("http://s3.amazonaws.com/assets.datacamp.com/production/course_1161/datasets/present_ex6.RData"))
+load(url("http://s3.amazonaws.com/assets.datacamp.com/production/course_1156/datasets/present_ex6.RData"))
 
 # Knit Rmd file
 #rmarkdown::render("lab1_ex7.2.Rmd")
