92b0afaa4b322e434ce7becbf4bb51bc85c852b9
Nick Carchedi <nick.carchedi@gmail.com>
2019-03-25 19:21:48 -0400

    update link

--- requirements.sh
+++ requirements.sh
@@ -2,7 +2,7 @@
 set -e
 
 # get zip of all files
-wget https://assets.datacamp.com/production/repositories/302/datasets/0f4b647afad30e9f8acef15df4d2b0cb2840daa4/all-files.zip
+wget https://assets.datacamp.com/production/repositories/302/datasets/b9177982f038025ad55127ebcd2f91c26f201afa/all-files.zip
 
 # unzip all files
 unzip all-files.zip -d $DATADIR/
